23fef64259d7c7c0b7e5ccf9eee63262b83fed5b
K4nishk <k4nishk@github.com>
2021-09-03 01:39:52 -0600

    Finished

--- script.py
+++ script.py
@@ -1,5 +1,14 @@
 from importlib.metadata import version
 import requests
 
-# print(version('requests'))
-print(requests.get("http://google.com"))
+def main():
+	#print(version('requests'))
+	#print(requests.get("http://google.com"))
+
+	url = "https://raw.githubusercontent.com/K4nishk/cmput404lab1/main/script.py?token=AIHLPKB5W5RRIZ24THRDJXLBGHHRY"
+	r = requests.get(url, allow_redirects=True)
+	print(r.content)
+
+	return 0
+
+main()
